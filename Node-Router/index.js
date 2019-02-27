7e24946f705e27c252fb8c2cdb7a4c3af51530dd
shravya <shravya@getambee.com>
2019-02-27 16:04:02 +0530

    adding env variable for heroku

--- Node-Router/index.js
+++ Node-Router/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.json({ type: 'application/json' }))
@@ -14,6 +14,6 @@ var things3=require('./things3');
 
 app.use('/things', things);
 app.use('/things2',things2);
-app.use('/things3',things3);
+app.use('/things3',things3); 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
